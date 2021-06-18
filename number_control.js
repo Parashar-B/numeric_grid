8957180a86e0ebfaa4eaf74fb6a5875e8a5c22fa
Parashar-B <parasharbharali21@gmail.com>
2021-06-18 23:48:57 +0530

    prime checker code added

--- number_control.js
+++ number_control.js
@@ -11,3 +11,49 @@ function show_me(z){
          case 'm' :document.getElementById("math_div").style.visibility="visible"; visible_div=document.getElementById("math_div"); break;
       }
 }
+function clear_all(){
+   document.getElementById("prime_check_input").value="";
+   disable_all_prime();
+}
+
+function check_btn_control(){
+   if(document.getElementById("prime_check_input").value=="")
+      disable_all_prime();
+   else
+      enable_check_prime_btn();
+
+}
+
+function disable_all_prime(){
+   document.getElementById("sl1").style.backgroundColor="white";
+   document.getElementById("prime_check_btn").disabled=true;
+   document.getElementById("prime_check_btn").style.backgroundColor="gray";
+   document.getElementById("sl2").style.backgroundColor="white";
+   document.getElementById("prime_result").style.backgroundColor="black";
+   document.getElementById("prime_result").innerHTML="";
+   document.getElementById("clear_btn").style.visibility="hidden";
+}
+
+function enable_check_prime_btn(){
+   document.getElementById("sl1").style.backgroundColor="orange";
+   document.getElementById("prime_check_btn").disabled=false;
+   document.getElementById("prime_check_btn").style.backgroundColor="orange";
+   document.getElementById("clear_btn").style.visibility="visible";
+}
+
+function prime_check_result(){
+   document.getElementById("sl2").style.backgroundColor="orange";
+   document.getElementById("prime_result").style.backgroundColor="green";
+   var x=document.getElementById("prime_check_input").value;
+   var f=0;
+   for(var i=2;i<=x;i++)
+   {
+      if(x%i==0)
+         f++;
+   }
+   if(f==1)
+      document.getElementById("prime_result").innerHTML="PRIME";
+   else
+   document.getElementById("prime_result").innerHTML="NOT PRIME";
+}
+
